a08470c7c496eb49fcb3209dea10ff9f953543a0
bomi <a51022866@gmail.com>
2018-12-26 20:34:45 +0900

    first commit

---
+++ src/main/java/com/wonder/bring/BringApplication.java
@@ -0,0 +1,14 @@
+package com.wonder.bring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BringApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/wonder/bring/BringApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wonder.bring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
